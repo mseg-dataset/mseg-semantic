da20fc0a7efb5a55b45cb658d18bcadfdf291e39
John Lambert <johnwlambert@gmail.com>
2020-06-24 18:30:18 -0400

    try to prevent duplicate printing

--- mseg_semantic/tool/inference_task.py
+++ mseg_semantic/tool/inference_task.py
@@ -78,10 +78,11 @@ def get_logger():
     logger_name = "main-logger"
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    fmt = "[%(asctime)s %(levelname)s %(filename)s line %(lineno)d %(process)d] %(message)s"
-    handler.setFormatter(logging.Formatter(fmt))
-    logger.addHandler(handler)
+    if not logger.handlers:
+	    handler = logging.StreamHandler()
+	    fmt = "[%(asctime)s %(levelname)s %(filename)s line %(lineno)d %(process)d] %(message)s"
+	    handler.setFormatter(logging.Formatter(fmt))
+	    logger.addHandler(handler)
     return logger
 
 logger = get_logger()
