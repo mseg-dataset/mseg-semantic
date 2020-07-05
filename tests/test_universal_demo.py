3b47d64bed4a64576a3e98e9d9ce245fd795ece9
John Lambert <johnwlambert@gmail.com>
2020-07-04 21:27:53 -0400

    add a unit test on the demo

--- tests/test_universal_demo.py
+++ tests/test_universal_demo.py
@@ -25,7 +25,7 @@ def test_run_universal_demo():
 		'config': f'{ROOT_}/mseg_semantic/config/test/default_config_${base_size}.yaml', 
 		#'model_path': f'{_ROOT}/pretrained-semantic-models/${model_name}/${model_name}.pth',
 		'model_path': '/srv/scratch/jlambert30/MSeg/pretrained-semantic-models/mseg-3m/mseg-3m.pth',
-		'input_file': 'test_data/temp_files/demo_images',
+		'input_file': f'{ROOT_}/test_data/temp_files/demo_images',
 		'model_name': 'mseg-3m',
 		'dataset': 'default',
 		'base_size': base_size,
@@ -37,6 +37,7 @@ def test_run_universal_demo():
 	}
 	args = SimpleNamespace(**d)
 	use_gpu = True
+	print(args)
 	run_universal_demo(args, use_gpu)
 
 	# assert a file exists
