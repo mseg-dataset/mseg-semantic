7efd633249408de0c9094b5f1c0bc0e296a421c6
John Lambert <johnwlambert@gmail.com>
2020-07-16 10:30:25 -0400

    clean up avg meter

--- mseg_semantic/utils/avg_meter.py
+++ mseg_semantic/utils/avg_meter.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 
 import numpy as np
+import torch
 import torch.distributed as dist
+from typing import List
 
 from mseg_semantic.utils.iou import intersectionAndUnion, intersectionAndUnionGPU
 
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
-    def __init__(self):
+    def __init__(self) -> None:
         self.reset()
 
-    def reset(self):
+    def reset(self) -> None:
         self.val = 0
         self.avg = 0
         self.sum = 0
         self.count = 0
 
-    def update(self, val, n=1):
+    def update(self, val, n: int = 1) -> None:
         self.val = val
         self.sum += val * n
         self.count += n
@@ -27,7 +29,7 @@ class SegmentationAverageMeter(AverageMeter):
     """ 
     An AverageMeter designed specifically for evaluating segmentation results.
     """
-    def __init__(self):
+    def __init__(self) -> None:
         """ Initialize object. """
         self.intersection_meter = AverageMeter()
         self.union_meter = AverageMeter()
@@ -51,7 +53,13 @@ class SegmentationAverageMeter(AverageMeter):
         self.accuracy = sum(self.intersection_meter.val) / (sum(self.target_meter.val) + 1e-10)
         self.intersection = 0.
 
-    def update_metrics_gpu(self, pred, target, num_classes, ignore_idx, is_distributed):
+    def update_metrics_gpu(
+        self,
+        pred: torch.Tensor,
+        target: torch.Tensor,
+        num_classes: int,
+        ignore_idx: int,
+        is_distributed: bool):
         """ 
             Args:
             -    pred
@@ -73,7 +81,7 @@ class SegmentationAverageMeter(AverageMeter):
         self.target_meter.update(target)
         self.accuracy = sum(self.intersection_meter.val) / (sum(self.target_meter.val) + 1e-10)
 
-    def get_metrics(self, exclude=False, exclude_ids=None):
+    def get_metrics(self, exclude: bool = False, exclude_ids: List[int] = None):
         """
             Args:
             -   None
@@ -89,25 +97,22 @@ class SegmentationAverageMeter(AverageMeter):
         accuracy_class = self.intersection_meter.sum / (self.target_meter.sum + 1e-10)
 
         if exclude:
-            mIoU = np.mean(self.exclusion(iou_class, exclude_ids))
-            mAcc = np.mean(self.exclusion(accuracy_class, exclude_ids))
+            mIoU = np.mean(exclusion(iou_class, exclude_ids))
+            mAcc = np.mean(exclusion(accuracy_class, exclude_ids))
 
-            print('original miou is:, ', np.mean(iou_class))
+            #print('original miou is:, ', np.mean(iou_class))
         else:
             mIoU = np.mean(iou_class)
             mAcc = np.mean(accuracy_class)
         allAcc = sum(self.intersection_meter.sum) / (sum(self.target_meter.sum) + 1e-10)
         return iou_class, accuracy_class, mIoU, mAcc, allAcc
 
-    def exclusion(self, array, ids): # id is excluded id
-        ''' take in array of iou/acc., return mean excluded values'''
 
-        total = len(array)
+def exclusion(array: np.ndarray, excluded_ids: List[int]) -> np.ndarray:
+    """ take in array of IoU/Acc., return non-excluded IoU/acc values """
+    all_ids = np.arange(array.size)
+    # valid indices --> take complement of set intersection
+    relevant_array = array[~np.in1d(all_ids, excluded_ids)]
+    return relevant_array
 
-        count = []
-        for i in range(total):
-            if i not in ids:
-                count += [i]
-
-        return array[count]
 
