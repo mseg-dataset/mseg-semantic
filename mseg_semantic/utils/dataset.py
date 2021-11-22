670b88242728cd7b1bd8c39a3e8617d62a440891
John Lambert <johnlambert@hayoungs-mbp.lan>
2021-11-22 12:17:51 -0500

    reformat python black

--- mseg_semantic/utils/dataset.py
+++ mseg_semantic/utils/dataset.py
@@ -15,26 +15,28 @@ Could duplicate samples here to reduce overhead between epochs.
 Modified from https://github.com/hszhao/semseg/blob/master/util/dataset.py
 """
 
-IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm']
+IMG_EXTENSIONS = [".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm"]
 
 
 def is_image_file(filename: str) -> bool:
+    """Check if file represents an image, by comparing against several known image file extensions."""
     filename_lower = filename.lower()
     return any(filename_lower.endswith(extension) for extension in IMG_EXTENSIONS)
 
 
-def make_dataset(split: str, data_root: str, data_list_fpath: str) -> List[Tuple[str,str]]:
-    """ Create list of (image file path, label file path) pairs.
-    
+def make_dataset(split: str, data_root: str, data_list_fpath: str) -> List[Tuple[str, str]]:
+    """Create list of (image file path, label file path) pairs.
+
     Args:
         split: string representing split of data set to use, must be either 'train','val','test'
         data_root: path to where data lives, and where relative image paths are relative to
         data_list_fpath: path to .txt file with relative image paths
+    
     Returns:
         image_label_list: list of 2-tuples, each 2-tuple is comprised of an absolute image path
             and an absolute label path
     """
-    assert split in ['train', 'val', 'test']
+    assert split in ["train", "val", "test"]
     if not os.path.isfile(data_list_fpath):
         raise (RuntimeError("Image list file do not exist: " + data_list_fpath + "\n"))
     image_label_list = []
@@ -43,8 +45,8 @@ def make_dataset(split: str, data_root: str, data_list_fpath: str) -> List[Tuple
 
     for line in list_read:
         line = line.strip()
-        line_split = line.split(' ')
-        if split == 'test':
+        line_split = line.split(" ")
+        if split == "test":
             if len(line_split) != 1:
                 raise (RuntimeError("Image list file read line error : " + line + "\n"))
             image_name = os.path.join(data_root, line_split[0])
@@ -54,14 +56,14 @@ def make_dataset(split: str, data_root: str, data_list_fpath: str) -> List[Tuple
                 raise (RuntimeError("Image list file read line error : " + line + "\n"))
             image_name = os.path.join(data_root, line_split[0])
             label_name = os.path.join(data_root, line_split[1])
-        '''
+        """
         following check costs some time
         if is_image_file(image_name) and is_image_file(label_name) and os.path.isfile(image_name) and os.path.isfile(label_name):
             item = (image_name, label_name)
             image_label_list.append(item)
         else:
             raise (RuntimeError("Image list file line error : " + line + "\n"))
-        '''
+        """
         item = (image_name, label_name)
         image_label_list.append(item)
     print("Checking image&label pair {} list done!".format(split))
@@ -70,9 +72,9 @@ def make_dataset(split: str, data_root: str, data_list_fpath: str) -> List[Tuple
 
 
 class SemData(Dataset):
-    def __init__(self, split: str = 'train', data_root: str = None, data_list: str = None, transform = None) -> None:
+    def __init__(self, split: str = "train", data_root: str = None, data_list: str = None, transform=None) -> None:
         """Dataloader class for semantic segmentation datasets.
-        
+
         Args:
             split: string representing split of data set to use, must be either 'train','val','test'
             data_root: path to where data lives, and where relative image paths are relative to
@@ -93,18 +95,16 @@ class SemData(Dataset):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # convert cv2 read image from BGR order to RGB order
         image = np.float32(image)
 
-        label = imageio.imread(label_path) # # GRAY 1 channel ndarray with shape H * W
+        label = imageio.imread(label_path)  # # GRAY 1 channel ndarray with shape H * W
         label = label.astype(np.int64)
-
 
         if image.shape[0] != label.shape[0] or image.shape[1] != label.shape[1]:
             raise (RuntimeError("Image & label shape mismatch: " + image_path + " " + label_path + "\n"))
         if self.transform is not None:
-            if self.split != 'test':
+            if self.split != "test":
                 image, label = self.transform(image, label)
             else:
                 # use dummy label in transform, since label unknown for test
                 image, label = self.transform(image, image[:, :, 0])
 
         return image, label
-
