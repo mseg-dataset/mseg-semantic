bc65a3aebc2beb22f1b9f84e7ca00223c21bd702
John Lambert <johnlambert@hayoungs-mbp.lan>
2021-11-22 11:49:38 -0500

    clean up docstrings for pixel normalization

--- mseg_semantic/utils/normalization_utils.py
+++ mseg_semantic/utils/normalization_utils.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
+from typing import Optional, Tuple
+
 import numpy as np
 import torch
 
-from typing import Optional, Tuple
 
+def get_imagenet_mean_std() -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
+    """See use here in Pytorch ImageNet script:
+    https://github.com/pytorch/examples/blob/master/imagenet/main.py#L197
 
-def get_imagenet_mean_std() -> Tuple[Tuple[float,float,float], Tuple[float,float,float]]:
-    """ See use here in Pytorch ImageNet script: 
-        https://github.com/pytorch/examples/blob/master/imagenet/main.py#L197
-
-        Returns:
-        -   mean: Tuple[float,float,float], 
-        -   std: Tuple[float,float,float] = None
+    Returns:
+        mean: average R,G,B values in [0,255] range
+        std: standard deviation of R,G,B values in [0,255] range
     """
     value_scale = 255
     mean = [0.485, 0.456, 0.406]
@@ -20,21 +20,18 @@ def get_imagenet_mean_std() -> Tuple[Tuple[float,float,float], Tuple[float,float
     std = [0.229, 0.224, 0.225]
     std = [item * value_scale for item in std]
     return mean, std
-            
 
-def normalize_img(  input: torch.Tensor, 
-                    mean: Tuple[float,float,float], 
-                    std: Optional[Tuple[float,float,float]] = None):
-    """ Pass in by reference Torch tensor, and normalize its values.
 
-        Args:
-        -   input: Torch tensor of shape (3,M,N), must be in this order, and
-                of type float (necessary).
-        -   mean: mean values for each RGB channel
-        -   std: standard deviation values for each RGB channel
+def normalize_img(
+    input: torch.Tensor, mean: Tuple[float, float, float], std: Optional[Tuple[float, float, float]] = None
+) -> None:
+    """Pass in by reference Torch tensor, and normalize its values.
 
-        Returns:
-        -   None
+    Args:
+        input: Torch tensor of shape (3,M,N), must be in this order, and
+            of type float (necessary).
+        mean: mean values for each RGB channel
+        std: standard deviation values for each RGB channel
     """
     if std is None:
         for t, m in zip(input, mean):
@@ -42,4 +39,3 @@ def normalize_img(  input: torch.Tensor,
     else:
         for t, m, s in zip(input, mean, std):
             t.sub_(m).div_(s)
-
