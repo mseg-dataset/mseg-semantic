4d6e859b27f0348d8f18b5fb28fc4c57b29ee42d
John Lambert <johnlambert@dept-143-215-34-103.vpn.gatech.edu>
2021-11-24 23:34:17 -0500

    remove unused code

--- mseg_semantic/model/seg_hrnet_config.py
+++ mseg_semantic/model/seg_hrnet_config.py
@@ -1,10 +1,7 @@
 
+"""Config definitions for the HRNet architecture."""
 
 from typing import List
-
-import hydra
-from hydra.utils import instantiate
-from omegaconf import OmegaConf
 
 from dataclasses import dataclass
 
@@ -30,31 +27,3 @@ class HRNetArchConfig:
   STAGE3: HRNetStageConfig
   STAGE4: HRNetStageConfig
   FINAL_CONV_KERNEL: int
-
-
-
-
-
-
-
-
-
-
-
-SceneOptimizer:
-  _target_: gtsfm.scene_optimizer.SceneOptimizer
-  save_gtsfm_data: True
-  save_two_view_correspondences_viz: False
-  save_3d_viz: True
-  pose_angular_error_thresh: 5 # degrees
-
-  feature_extractor:
-    _target_: gtsfm.feature_extractor.FeatureExtractor
-    detector_descriptor:
-      _target_: gtsfm.frontend.cacher.detector_descriptor_cacher.DetectorDescriptorCacher
-      detector_descriptor_obj:
-        _target_: gtsfm.frontend.detector_descriptor.superpoint.SuperPointDetectorDescriptor
-
-
-
-
